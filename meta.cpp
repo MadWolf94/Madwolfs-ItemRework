8c27c144ef6e1e4b5dbbacf794f27009cd634f47
MadWolf94 <madguy4894@gmail.com>
2022-06-05 18:40:57 +0100

    Add files via upload

---
+++ meta.cpp
@@ -0,0 +1,4 @@
+protocol = 1;
+publishedid = 2685445852;
+name = "Madwolfs' Med-bag Rework";
+timestamp = 5249407904241116603;
---
+++ mod.cpp
@@ -0,0 +1,11 @@
+type = "mod"; 
+name = "MadWolfs-ItemRework";
+picture = "@Madwolfs-ItemRework/addons/Attributes_Edits/gui/imagesets/MW_logo.paa";
+logoSmall = "@Madwolfs-ItemRework/addons/Attributes_Edits/gui/imagesets";
+logo = "@Madwolfs-ItemRework/addons/Attributes_Edits/gui/imagesets/MW_logo.paa";
+logoOver = "@Madwolfs-ItemRework/addons/Attributes_Edits/gui/imagesets/MW_logo.paa";
+tooltip = "tooltip";
+overview = "Just Some Things I Put Together";
+action = "https://steamcommunity.com/sharedfiles/filedetails/?id=2685445852";
+author = "Madwolf";
+version = "1.0";
